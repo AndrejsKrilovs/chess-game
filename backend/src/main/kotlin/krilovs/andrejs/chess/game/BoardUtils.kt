31791538fb521b3e5ca9513008e7855dda58ff66
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2026-04-04 02:31:02 +0300

    (fix): board utils logic

--- backend/src/main/kotlin/krilovs/andrejs/chess/game/BoardUtils.kt
+++ backend/src/main/kotlin/krilovs/andrejs/chess/game/BoardUtils.kt
@@ -2,17 +2,30 @@ package krilovs.andrejs.chess.game
 
 class BoardUtils {
   fun getDiagonalCoordinatesInRange(source: Coordinates, destination: Coordinates): List<Coordinates> {
-    val fileShift = if (source.file.code < destination.file.code) 1 else -1
-    val rankShift = if (source.rank < destination.rank) 1 else -1
+    val fileStep = (destination.file.code - source.file.code).coerceIn(-1, 1)
+    val rankStep = (destination.rank - source.rank).coerceIn(-1, 1)
 
-    var current = source
-    return buildList {
-      while (true) {
-        val next = current.shift(CoordinatesShift(fileShift, rankShift)) ?: break
-        if (next == destination) break
-        add(next)
-        current = next
-      }
-    }
+    return generateSequence(source) { it.shift(CoordinatesShift(fileStep, rankStep)) }
+      .drop(1)
+      .takeWhile { it != destination }
+      .toList()
+  }
+
+  fun getVerticalCoordinatesInRange(source: Coordinates, destination: Coordinates): List<Coordinates> {
+    val step = (destination.rank - source.rank).coerceIn(-1, 1)
+
+    return generateSequence(source) { it.shift(CoordinatesShift(0, step)) }
+      .drop(1)
+      .takeWhile { it != destination }
+      .toList()
+  }
+
+  fun getHorizontalCoordinatesInRange(source: Coordinates, destination: Coordinates): List<Coordinates> {
+    val step = (destination.file.code - source.file.code).coerceIn(-1, 1)
+
+    return generateSequence(source) { it.shift(CoordinatesShift(step, 0)) }
+      .drop(1)
+      .takeWhile { it != destination }
+      .toList()
   }
 }
--- backend/src/main/kotlin/krilovs/andrejs/chess/piece/Rook.kt
+++ backend/src/main/kotlin/krilovs/andrejs/chess/piece/Rook.kt
@@ -1,11 +1,28 @@
 package krilovs.andrejs.chess.piece
 
+import krilovs.andrejs.chess.game.Board
+import krilovs.andrejs.chess.game.BoardUtils
 import krilovs.andrejs.chess.game.Color
 import krilovs.andrejs.chess.game.Coordinates
 import krilovs.andrejs.chess.game.CoordinatesShift
 
 class Rook(color: Color, coordinates: Coordinates) : Piece(color, coordinates) {
   override fun getPieceMoves(): Set<CoordinatesShift> {
-    return setOfNotNull()
+    return buildSet {
+      (-7 .. 7).filter { it != 0 }.forEach {
+        add(CoordinatesShift(it, 0))
+        add(CoordinatesShift(0, it))
+      }
+    }
+  }
+
+  override fun isSquareAvailable(coord: Coordinates, board: Board): Boolean {
+    return super.isSquareAvailable(coord, board) &&
+      BoardUtils()
+        .getVerticalCoordinatesInRange(coordinates, coord)
+        .none { board.getPiece(it) != null } &&
+      BoardUtils()
+        .getHorizontalCoordinatesInRange(coordinates, coord)
+        .none { board.getPiece(it) != null }
   }
 }
